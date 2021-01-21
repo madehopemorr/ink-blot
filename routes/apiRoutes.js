0b7ffbfe47b5f6384f772cabdad889c6bd487427
Madeline Morris <madehopemorr@gmail.com>
2021-01-21 15:04:31 -0800

    apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const store = require("../db/store");
 
 router.get("/notes", (req, res) => {
+    console.log("get routes")
     store
         .getNotes()
         .then((notes) => res.json(note))
