90e79f0232a3b5bae5586d0165f9e44a71408372
Madeline Morris <madehopemorr@gmail.com>
2021-01-21 00:52:24 -0800

    update error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api, apiRoutes");
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
